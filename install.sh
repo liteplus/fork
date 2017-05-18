5194badb84eebf5d8977950a9597e7a2b81260c6
liteplus <Cryptobot07@gmail.com>
2017-05-18 22:23:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev libdb-dev libdb++-dev libboost-all-dev git libssl1.0.0-dbg -y
 sudo apt-get install libdb-dev libdb++-dev libboost-all-dev libminiupnpc-dev libminiupnpc-dev libevent-dev libcrypto++-dev libgmp3-dev -y
-git clone https://github.com/liteplus/liteplus liteplus
-cd liteplus
 cd src
 make -f makefile.unix
