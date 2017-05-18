2f6f438700860e00c4a0e7c669b45595c9fd7987
liteplus <Cryptobot07@gmail.com>
2017-05-18 23:05:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo apt-get update
 sudo apt-get install build-essential libssl-dev libdb-dev libdb++-dev libboost-all-dev git libssl1.0.0-dbg -y
 sudo apt-get install libdb-dev libdb++-dev libboost-all-dev libminiupnpc-dev libminiupnpc-dev libevent-dev libcrypto++-dev libgmp3-dev -y
 cd src
+mkdir -p obj/zerocoin && chmod +x leveldb/build_detect_platform
 make -f makefile.unix
