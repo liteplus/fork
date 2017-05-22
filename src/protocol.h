8c6b8f4e73bfda036f7c9836080f1517b18dc055
liteplus <Cryptobot07@gmail.com>
2017-05-22 16:18:51 +0300

    Update protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 27869 : 17869;
+    return testnet ? 27871 : 17849;
 }
 
 
